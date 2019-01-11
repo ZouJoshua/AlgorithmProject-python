3c45c5aa45f29c140eefc7fa9fadfa6be305f34b
zoushuai <zoushuai@git.apuscn.com>
2019-01-11 20:31:41 +0800

    add check bad case

--- nlp/classification/model_with_fasttext/check_bad_case.py
+++ nlp/classification/model_with_fasttext/check_bad_case.py
@@ -14,10 +14,10 @@ root_path = dirname(dirname(dirname(dirname(os.path.realpath(__file__)))))
 class_path = dirname(dirname(os.path.realpath(__file__)))
 sys.path.append(root_path)
 sys.path.append(dirname(class_path))
-
+from IPython.display import clear_output
 import json
 
-class check(object):
+class Check(object):
 
     def __init__(self, checkfile, check_outfile, level='two_level', subcategory='law', check_type='all', filter_proba=1):
         self.cf = checkfile
@@ -58,24 +58,46 @@ class check(object):
                 out = '正在检查：\n' \
                       'ID：{}\n' \
                       'Title：{}\n' \
-                      'Content：{}\n' \
+                      'Content：\n{}\n' \
                       '人工标注分类：{}\n' \
                       '机器预测分类：{}\n' \
                       '机器预测分类概率：{}\n'.format(line_dict["article_id"], line_dict["title"],
                                              line_dict['content'], line_dict[self.level],
                                              line_dict['predict_two_level'], p)
-                if KeyboardInterrupt:
-                    with open(self.cof, 'a', encoding='utf-8') as outf:
-                        for i in check_out:
-                            outf.write(json.dumps(i))
-                            outf.write('\n')
-                    return
                 yield out
-                print("请检查，输入 'y' or 'n'")
-                is_right = input()
+                is_right = input("请检查，输入 'y' or 'n'\n")
                 if is_right.lower() in ['y', 'n']:
                     out['is_right'] = is_right
                     check_out.append(out)
             continue
 
 
+checkfile="/data/zoushuai/news_content/sub_classification_model/national_v0/national_model_1/data/train_check_pred.json"
+checkoutfile = "/data/zoushuai/news_content/sub_classification_model/national_v0/national_model_1/check.json"
+c = Check(checkfile, checkoutfile, level='two_level', subcategory='law', check_type='right', filter_proba=1)
+bucket = c.get_check_data()
+c_test = c.check(bucket)
+
+
+check_out = list()
+File = open(checkoutfile, 'a+')
+while True:
+    try:
+        clear_output()
+        sys.stdout.flush()
+        out = bucket[-1]
+        print(next(c.check(bucket)))
+#         print(len(check_out))
+        is_right = input("请检查，输入 'y' or 'n'\n")
+        if is_right.lower() in ['y', 'n']:
+            out['is_right'] = is_right
+            if is_right == 'y':
+                out["two_level_checked"] = ''
+            else:
+                out['two_level_checked'] = input("请输入正确类别：{1:crime, 2:law, 3:education, 4:politics, 5:others}\n")
+            File.write(json.dumps(out) + '\n')
+            File.flush()
+        continue
+    except StopIteration:
+        File.close()
+        sys.exit()
