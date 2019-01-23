0876a050d705af94b77276b304f3092f5289b282
zoushuai <zoushuai@git.apuscn.com>
2019-01-23 11:34:58 +0800

    fix label map

--- nlp/classification/model_evaluate/check_random.py
+++ nlp/classification/model_evaluate/check_random.py
@@ -9,8 +9,22 @@
 
 import random
 import json
+
 def check_random(file, newfile):
     f = open(file, 'r', encoding='utf-8')
+    outf = open(newfile, 'w', encoding='utf-8')
     lines = f.readlines()
+    random.shuffle(lines)
     check_lines = random.sample(lines, 100)
+    for line in check_lines:
+        outf.write(line)
+        outf.write('\n')
+        outf.flush()
+    f.close()
+    outf.close()
     return
+
+
+file = '/data/zoushuai/news_content/sub_classification_model/business/business_model_3/data/test_check_pred.json'
+newfile = '/data/zoushuai/news_content/check/business_test_check_random'
+check_random(file, newfile)
