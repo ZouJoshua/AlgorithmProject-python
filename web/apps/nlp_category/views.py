19f9251f5f8f2b39dbd3fb87d5864f8824436b22
zoushuai <zoushuai@git.apuscn.com>
2019-03-04 19:00:39 +0800

    update nlp web

--- web/apps/nlp_category/views.py
+++ web/apps/nlp_category/views.py
@@ -4,18 +4,20 @@ from django.views import View
 import datetime
 from .classification.load_model import LoadModel
 from .classification.predict import Predict
-from web.utils.logger import Logger
-from web.web.settings import PROJECT_LOG_FILE, NLP_MODEL_PATH
 
 import os
 from os.path import dirname
 import sys
 
-root_path = dirname(dirname(dirname(dirname(os.path.realpath(__file__)))))
+root_path = dirname(dirname(dirname(os.path.realpath(__file__))))
 apps_path = dirname(dirname(os.path.realpath(__file__)))
 sys.path.append(root_path)
 sys.path.append(dirname(apps_path))
 sys.path.append(apps_path)
+
+from web.utils.logger import Logger
+from web.web.settings import PROJECT_LOG_FILE, NLP_MODEL_PATH
+
 
 logger = Logger('nlp_category_predict', log2console=False, log2file=True, logfile=PROJECT_LOG_FILE).get_logger()
 
