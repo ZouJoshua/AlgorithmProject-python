59fbd7c7d085a0c988c3b321a6b452b282c42cb5
ZouJoshua <joshua_zou@163.com>
2019-11-28 19:04:21 +0800

    update project

--- ba_sort/sort.py
+++ ba_sort/sort.py
@@ -9,21 +9,56 @@
 """
 
 
+## 直接插入排序
+def insert_sort(array):
+    """
+    算法复杂度：
+    时间复杂度：O(n2),O(n2),O(n)
+    空间复杂度：O(I)
+    稳定性：稳定
+    :param array:
+    :return:
+    """
+    for i in range(len(array)):
+        for j in range(i):
+            if array[i] < array[j]:
+                array.insert(j, array.pop(i))
+                break
+    return array
 
-def bubble_sort(ary):
-    l = len(ary)
-    pass
+## 希尔排序
+def shell_sort(array):
+    """
+    算法复杂度：
+    时间复杂度：O(n1.3),O(n2),O(n)
+    空间复杂度：O(I)
+    稳定性：不稳定
+    :param array:
+    :return:
+    """
+    gap = len(array)
+    while gap > 1:
+        gap = gap // 2
+        for i in range(gap, len(array)):
+            for j in range(i % gap, i, gap):
+                if array[i] < array[j]:
+                    array[i], array[j] = array[j], array[i]
+    return array
 
-
-def selece_sort(ary):
-    pass
-
-
-def merge_sort(ary):
-    pass
-
-
-def quick_sort(ary):
-    pass
+## 冒泡排序
+def bubble_sort(array):
+    """
+    算法复杂度：
+    时间复杂度：O(n2),O(n2),O(n)
+    空间复杂度：O(I)
+    稳定性：不稳定
+    :param array:
+    :return:
+    """
+    for i in range(len(array)):
+        for j in range(i, len(array)):
+            if array[i] > array[j]:
+                array[i], array[j] = array[j], array[i]
+    return array
 
 
