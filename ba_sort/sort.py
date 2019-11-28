edbbf38bca3407dfdac4a229d094b7b635988659
ZouJoshua <joshua_zou@163.com>
2019-11-29 00:03:15 +0800

    update project

--- ba_sort/sort.py
+++ ba_sort/sort.py
@@ -46,19 +46,53 @@ def shell_sort(array):
     return array
 
 ## 冒泡排序
-def bubble_sort(array):
+
+def bubble_sort_v1(arr):
     """
     算法复杂度：
-    时间复杂度：O(n2),O(n2),O(n)
-    空间复杂度：O(I)
-    稳定性：不稳定
-    :param array:
+    时间复杂度：O(n2),O(n),O(n2)
+    空间复杂度：O(1)
+    稳定性：稳定
+    :param arr:
     :return:
     """
-    for i in range(len(array)):
-        for j in range(i, len(array)):
-            if array[i] > array[j]:
-                array[i], array[j] = array[j], array[i]
-    return array
+    for i in range(1, len(arr)):
+        for j in range(len(arr) - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def bubble_sort(arr):
+    """
+    :param arr:
+    :return:
+    """
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
+## 选择排序
+
+def select_sort(arr):
+    """
+    算法复杂度：
+    时间复杂度：O(n2),O(n2),O(n2)
+    空间复杂度：O(I)
+    稳定性：不稳定
+    :param arr:
+    :return:
+    """
+    n = len(arr)
+    for i in range(n-1):
+        # 记录最小数的索引
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # i不是最小数时，将i和最小数进行交换
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
